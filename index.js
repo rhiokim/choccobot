4fe23e060313c718393d5afb759a106052327e94
rhiokim <rhio.kim@gmail.com>
2012-12-07 18:05:15 +0900

    raw 데이터 게더링 로직 초기
    raw 데이터 게더링 로직 초기

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 var fs = require('fs'),
+    path = require('path'),
     util = require('util'),
-    twitter = require('twitter');
+    twitter = require('twitter'),
+    moment = require('moment'),
+    mkdirp = require('mkdirp'),
+    JSONStream = require('JSONStream'),
+    config = require('./config');
 
-var config = {
-  twitter: {
-        consumer_key: 'QmQN45XwTND5NtUouShPEw',
-        consumer_secret: 'eN1lHIxWPAt4kChgtpp19ehtsvs6dp2m2YtjAfb3HAE',
-        access_token_key: '27830793-H0gWJ4Y6bsOACJnkXelAatKOWreHjHR5T8w73ubiH',
-        access_token_secret: 'mPs0eWOj0lGcEcXA4wbmPPyX3eb5Njw3Sb5jVhWD0'
-  },
-  track: [
-    'node.js'
-  ] 
-};
+var track = 'node.js';
+var date = moment().format('YYYY-MM-DD');
+var dir = path.join('./.raw', date, track);
+
+mkdirp.sync(dir);
+
+var raw = dir +'/index.markdown';
+var res = fs.existsSync(raw);
+
+var writeStream = fs.createWriteStream(raw);
 
 var twit = new twitter(config.twitter);
-twit.stream('statuses/filter', {
-  track: config.track.join(',')
-}, function(stream) {
-  stream.on('data', onData)
-});
+    twit.stream('statuses/filter', {
+      track: track
+    }, function(stream) {
+      stream.on('data', onData);
+    });
 
+function checkStore() {
+  data = moment().format('YYYY-MM-DD');
+}
 function onData(tweet) {
-  console.log(tweet)
+  writeStream.write(JSON.stringify(tweet, null, 2) +',\n');
+}
+
+function start() {
 }
